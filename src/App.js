0dd5b22153733d032dc30ba27081c22b4efd4141
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-03-13 20:11:22 -0600

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ApiData';
+import './style.css';
+import './index.html';
+
+function App() {
+  return (
+    <div className="App">
+      <App/>
+    </div>
+  );
+}
+
+export default App;
